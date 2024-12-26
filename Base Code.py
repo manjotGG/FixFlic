e58f4d2c1c5e65978cd6749a5ca06cc6c0dd9dbf
manjotGG <manjotcharanjeet@gmail.com>
2024-12-26 14:50:24 +0530

    Initial commit

---
+++ Base Code.py
@@ -0,0 +1,20 @@
+import requests
+import random
+
+API_KEY = "fd75f2cbedc1bf37544712100425c13f"
+BASE_URL = "https://api.themoviedb.org/3"
+
+def Suprise_me():
+    url = f"{BASE_URL}/movie/popular"  # Corrected endpoint
+    params = {"api_key": API_KEY, "language": "en-US", "page": 1}
+    response = requests.get(url, params=params)
+    data = response.json()
+    movie = data.get("results",[])
+    if movie:
+        random_movie = random.choice(movie)
+        return f"{random_movie['title']} ({random_movie['release_date'][:4]})"
+    else:
+        return f"Error:"
+
+movie = Suprise_me()
+print("Popular Movies:", movie)
